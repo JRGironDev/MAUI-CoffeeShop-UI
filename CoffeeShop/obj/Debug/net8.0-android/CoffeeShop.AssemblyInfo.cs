b75dcdd57c5237589df525f5f854023e17843de9
José Quiñónez <joserobertoq@outlook.com>
2024-04-08 06:29:19 -0600

    Add Border

--- CoffeeShop/obj/Debug/net8.0-android/CoffeeShop.AssemblyInfo.cs
+++ CoffeeShop/obj/Debug/net8.0-android/CoffeeShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0270dbffabcde5e2c8932ab6c98cea959ecde28a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
